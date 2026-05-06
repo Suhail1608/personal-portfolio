586689856e86ce817471e90e329d3f150b7e2728
Suhail1608 <suhailbasha1608@gmail.com>
2026-05-06 20:01:07 +0530

    dotted bg

--- src/app/page.js
+++ src/app/page.js
@@ -11,6 +11,7 @@ export default function Page() {
       <div className="absolute inset-0 -z-10">
         <div className="absolute top-1 left-1/2 w-75 h-75 md:w-125 md:h-125 bg-primary/20 blur-[120px] rounded-full -translate-x-1/2"></div>
       </div>
+      <div className="absolute inset-0 -z-10 bg-[radial-gradient(rgba(245,158,11,0.25)_1px,transparent_1px)] bg-size-[24px_24px] mask-[linear-gradient(to_bottom,black,transparent)]" />
       <Home />
       <Projects />
       <Skills />
